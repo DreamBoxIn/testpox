8ede13214072a2b82f029df3a62a7f961f8731b6
mtrunkbear <migueltroncosogaldames@gmail.com>
2024-08-08 20:28:48 -0400

    removed comments

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Habilita el modo estricto de React para detectar problemas potenciales
-  swcMinify: true,       // Habilita la minificación usando SWC para construir el proyecto más rápido
+  reactStrictMode: true,
+  swcMinify: true,     
 
   // Configuración de optimización de imágenes
   images: {
-    domains: ['example.com'], // Reemplaza con los dominios desde los que cargarás imágenes externas
-    deviceSizes: [320, 420, 768, 1024, 1200], // Configura tamaños de dispositivo
-    imageSizes: [16, 32, 48, 64, 96],         // Configura tamaños de imagen
+    domains: ['example.com'],
+    deviceSizes: [320, 420, 768, 1024, 1200], 
+    imageSizes: [16, 32, 48, 64, 96],         
   },
 
   // Configuración de encabezados para CORS y CSP
   async headers() {
     return [
       {
-        source: '/(.*)', // Aplica a todas las rutas
+        source: '/(.*)', 
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: '*', // Permite acceso desde cualquier origen
+            value: '*', 
           },
           {
             key: 'Access-Control-Allow-Methods',
