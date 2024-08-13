ddadcafb079957172ebf724a4f090e58a0c56130
BenjaDreamBox <b.lattanzimerino@gmail.com>
2024-08-13 03:19:05 -0400

    PXO-29: Ajustes en WalletUIComponent para reorganizar botones e implementar iconos

--- src/components/WalletUIComponent/WalletUIComponent.tsx
+++ src/components/WalletUIComponent/WalletUIComponent.tsx
@@ -2,16 +2,25 @@
 
 import React from 'react';
 import { Card, Button } from 'antd';
+import { ShoppingOutlined, DownloadOutlined, UploadOutlined, DollarOutlined } from '@ant-design/icons';
 import styles from './WalletUIComponent.module.scss';
 
 const WalletUIComponent: React.FC = () => {
   return (
     <Card className={styles.walletCard}>
       <div className={styles.buttonGroup}>
-        <Button type="primary" className={styles.walletButton}>Comprar</Button>
-        <Button type="primary" className={styles.walletButton}>Recibir</Button>
-        <Button type="primary" className={styles.walletButton}>Enviar</Button>
-        <Button type="primary" className={styles.walletButton}>Vender</Button>
+        <Button type="primary" className={styles.walletButton} icon={<ShoppingOutlined />}>
+          Comprar
+        </Button>
+        <Button type="primary" className={styles.walletButton} icon={<DownloadOutlined />}>
+          Recibir
+        </Button>
+        <Button type="primary" className={styles.walletButton} icon={<UploadOutlined />}>
+          Enviar
+        </Button>
+        <Button type="primary" className={styles.walletButton} icon={<DollarOutlined />}>
+          Vender
+        </Button>
       </div>
     </Card>
   );
