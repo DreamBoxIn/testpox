3c54b8034cc57cffd93c3ccf048394d726807ef2
BenjaDreamBox <b.lattanzimerino@gmail.com>
2024-08-14 02:05:09 -0400

    Descripción clara de los cambios realizados

--- src/components/TitleSubtitleComponent/TitleSubtitleComponent.tsx
+++ src/components/TitleSubtitleComponent/TitleSubtitleComponent.tsx
@@ -4,8 +4,8 @@ import styles from './TitleSubtitleComponent.module.scss';
 const TitleSubtitleComponent: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>PXO Token</h1>
-      <h2 className={styles.subtitle}>La stablecoin basada en el peso MXN.</h2>
+      <h1 className={styles.title}>Intercambio</h1>
+      <h2 className={styles.subtitle}>Entre monedas estales 1:1 con MXN y USD</h2>
     </div>
   );
 };
